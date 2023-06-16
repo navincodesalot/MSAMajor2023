0b6dae9a286323de8991b44e30ef15a86c6e58a8
Navin Narayanan <nan.navinnarayanan@gmail.com>
2023-06-16 11:21:30 -0500

    interpreter.py

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,17 +1,35 @@
-cool = input("Enter a problem: ")
-cool = cool.split(" ")
-x = float(cool[0])
-y = cool[1]
-z = float(cool[2])
+def main():
+    while True:
+        cool = input("Enter a problem: ")
+        cool = cool.split(" ")
+        if len(cool) != 3:
+            print("Error: Invalid Format")
+            continue
 
-sum = float(0)
+        x = int(cool[0])
+        y = cool[1]
+        z = int(cool[2])
 
-if y == "+":
-    sum = x + z
-elif y == "-":
-    sum = x - z
-elif y == "*":
-    sum = x * z
-elif y == "/":
-    sum = x / z
-print(f"{sum:0.1f}")
+        if z not in ["+", "-", "*", "/"]:
+            print("Invalid Operator")
+            continue
+        
+        if z == 0:
+            print("Divide by Zero Error")
+            continue
+        sum = int(0)
+        
+        if y == "+":
+            sum = x + z
+        elif y == "-":
+            sum = x - z
+        elif y == "*":
+            sum = x * z
+        elif y == "/":
+            sum = x / z
+
+        print(f"{sum:0.1f}")
+        another_calc = input("Would you like to evaluate another expression? Y to continue, any other key to exit ").lower()
+        if another_calc != "y":
+            break
+main()
