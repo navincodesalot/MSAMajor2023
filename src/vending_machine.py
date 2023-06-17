467faa184a975ed507e205ce5addc57622360a01
Navin Narayanan <nan.navinnarayanan@gmail.com>
2023-06-17 10:11:40 -0500

    remove extra condition

--- src/vending_machine.py
+++ src/vending_machine.py
@@ -2,7 +2,7 @@ def main():
     amount = int(50)
     accepted_coins = [1, 5, 10, 25]
     inserted_coins = int(0)
-    while amount != 0:
+    while amount > 0:
         try:
             print(f"Amount Due: {amount}")
             inserted_coins = int(input("Insert coin: "))
@@ -10,9 +10,7 @@ def main():
                 continue
             else:
                 amount -= inserted_coins
-            if amount <= 0:
-                print(f"Change owed: {abs(amount)}")
-                break
         except ValueError:
             continue;
+    print(f"Change owed: {abs(amount)}")
 main()
