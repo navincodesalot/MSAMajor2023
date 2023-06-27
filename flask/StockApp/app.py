c65a4c2f46d6074d93963443157a34485aa81d7a
Navin Narayanan <nan.navinnarayanan@gmail.com>
2023-06-27 11:17:17 -0500

    finish stockapp

--- flask/StockApp/app.py
+++ flask/StockApp/app.py
@@ -21,82 +21,127 @@ def index():
         timeSeries = request.form.get("time")
         startDate = (request.form.get("start_date"))
         endDate = (request.form.get("end_date"))
-        sd = startDate.split("-")
-        ed = endDate.split("-")
-        sd = datetime.date(int(sd[0]), int(sd[1]), int(sd[2]))
-        ed = datetime.date(int(ed[0]), int(ed[1]), int(ed[2]))
         data, line_chart = None, None
         open_info = []
         high_info = []
         low_info = []
         close_info = []
+        time_info = []
 
-        daysInDaily = []
-        if sd > ed:
+        if startDate > endDate:
             flash('End Date cannot begin before start date')
             return redirect(url_for('index'))
 
         if timeSeries.lower() == "intraday":
             data = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=15min&apikey=' + av_key).json()
-            # open_info.insert(0, float(data["Time Series (15min)"]["1. open"]))
-            # high_info.insert(0, float(data["Time Series (15min)"]["2. high"]))
-            # low_info.insert(0, float(data["Time Series (15min)"]["3. low"]))
-            # close_info.insert(0, float(data["Time Series (15min)"]["4. close"]))
-            # for d in data["Time Series (15min)"]:
-            #     if startDate in d:
-            #         chartDates = d
-            #         # print(data["Time Series (15min)"])
-            # if chartType == "bar":
-            #     pass
-            # elif chartType == "line":
-            #     line_chart = pygal.line()
-            #     line_chart.Line(legend_box_size=18, title=f'Stock Data for {symbol}: {startDate} to {endDate}')
-            #     line_chart.x_labels = chartDates
-            #     line_chart.add('Open', open_info)
-            #     line_chart.add('High', high_info)
-            #     line_chart.add('Low', low_info)
-            #     line_chart.add('Close', close_info)
-            #     line_chart.render_data_uri()
-
-        elif timeSeries.lower() == "daily":
-            data = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey=' + av_key).json()
-            newStartDate = sd
-            daysInDaily.append(newStartDate)
-            for _ in range((ed - sd).days):
-                newStartDate += datetime.timedelta(days=1)
-                newStartDate.strftime('%Y/%m/%d')
-                daysInDaily.append(newStartDate)
-            c = 0
-            for d in data["Time Series (Daily)"]:
-                open_info.insert(d, float(data["Time Series (Daily)"][daysInDaily[c]]["1. open"]))
-                high_info.insert(d, float(data["Time Series (Daily)"][daysInDaily[c]]["2. high"]))
-                low_info.insert(d, float(data["Time Series (Daily)"][daysInDaily[c]]["3. low"]))
-                close_info.insert(d, float(data["Time Series (Daily)"][daysInDaily[c]]["4. close"]))
-                c += 1
+            for d in data["Time Series (15min)"]:
+                
+                if d.split(" ")[0] >= startDate and d.split(" ")[0] <= endDate:
+                    time_info.insert(0, d)
+                    open_info.insert(0, float(data["Time Series (15min)"][d]["1. open"]))
+                    high_info.insert(0, float(data["Time Series (15min)"][d]["2. high"]))
+                    low_info.insert(0, float(data["Time Series (15min)"][d]["3. low"]))
+                    close_info.insert(0, float(data["Time Series (15min)"][d]["4. close"]))
             if chartType == "bar":
-                pass
-            elif chartType == "line":
-                line_chart = pygal.line()
-                line_chart.Line(legend_box_size=18, title=f'Stock Data for {symbol}: {startDate} to {endDate}')
-                line_chart.x_labels = daysInDaily
+                line_chart = pygal.Bar()
+                line_chart.title = f'Stock Data for {symbol}: {startDate} to {endDate}'
+                line_chart.x_labels = time_info
                 line_chart.add('Open', open_info)
                 line_chart.add('High', high_info)
                 line_chart.add('Low', low_info)
                 line_chart.add('Close', close_info)
-                line_chart.render_data_uri()
-                pass
+                line_chart = line_chart.render_data_uri()
+            elif chartType == "line":
+                line_chart = pygal.Line()
+                line_chart.title = f'Stock Data for {symbol}: {startDate} to {endDate}'
+                line_chart.x_labels = time_info
+                line_chart.add('Open', open_info)
+                line_chart.add('High', high_info)
+                line_chart.add('Low', low_info)
+                line_chart.add('Close', close_info)
+                line_chart = line_chart.render_data_uri()
+        elif timeSeries.lower() == "daily":
+            data = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey=' + av_key).json()
+            for d in data["Time Series (Daily)"]:
+                if d >= startDate and d <= endDate:
+                    time_info.insert(0, d)
+                    open_info.insert(0, float(data["Time Series (Daily)"][d]["1. open"]))
+                    high_info.insert(0, float(data["Time Series (Daily)"][d]["2. high"]))
+                    low_info.insert(0, float(data["Time Series (Daily)"][d]["3. low"]))
+                    close_info.insert(0, float(data["Time Series (Daily)"][d]["4. close"]))
+            if chartType == "bar":
+                line_chart = pygal.Bar()
+                line_chart.title = f'Stock Data for {symbol}: {startDate} to {endDate}'
+                line_chart.x_labels = time_info
+                line_chart.add('Open', open_info)
+                line_chart.add('High', high_info)
+                line_chart.add('Low', low_info)
+                line_chart.add('Close', close_info)
+                line_chart = line_chart.render_data_uri()
+            elif chartType == "line":
+                line_chart = pygal.Line()
+                line_chart.title = f'Stock Data for {symbol}: {startDate} to {endDate}'
+                line_chart.x_labels = time_info
+                line_chart.add('Open', open_info)
+                line_chart.add('High', high_info)
+                line_chart.add('Low', low_info)
+                line_chart.add('Close', close_info)
+                line_chart = line_chart.render_data_uri()
+                
         elif timeSeries.lower() == "weekly":  
             data = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={symbol}&apikey=' + av_key).json()
-            open_info.insert(0, float(data["Time Series (Weekly)"][startDate]["1. open"]))
-            high_info.insert(0, float(data["Time Series (Weekly)"][startDate]["2. high"]))
-            low_info.insert(0, float(data["Time Series (Weekly)"][startDate]["3. low"]))
-            close_info.insert(0, float(data["Time Series (Weekly)"][startDate]["4. close"]))
+            for d in data["Weekly Adjusted Time Series"]:
+                if d >= startDate and d <= endDate:
+                    time_info.insert(0, d)
+                    open_info.insert(0, float(data["Weekly Adjusted Time Series"][d]["1. open"]))
+                    high_info.insert(0, float(data["Weekly Adjusted Time Series"][d]["2. high"]))
+                    low_info.insert(0, float(data["Weekly Adjusted Time Series"][d]["3. low"]))
+                    close_info.insert(0, float(data["Weekly Adjusted Time Series"][d]["4. close"]))
+            if chartType == "bar":
+                line_chart = pygal.Bar()
+                line_chart.title = f'Stock Data for {symbol}: {startDate} to {endDate}'
+                line_chart.x_labels = time_info
+                line_chart.add('Open', open_info)
+                line_chart.add('High', high_info)
+                line_chart.add('Low', low_info)
+                line_chart.add('Close', close_info)
+                line_chart = line_chart.render_data_uri()
+            elif chartType == "line":
+                line_chart = pygal.Line()
+                line_chart.title = f'Stock Data for {symbol}: {startDate} to {endDate}'
+                line_chart.x_labels = time_info
+                line_chart.add('Open', open_info)
+                line_chart.add('High', high_info)
+                line_chart.add('Low', low_info)
+                line_chart.add('Close', close_info)
+                line_chart = line_chart.render_data_uri()
         elif timeSeries.lower() == "monthly":
             data = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={symbol}&apikey=' + av_key).json()
-            open_info.insert(0, float(data["Time Series (Monthly)"][startDate]["1. open"]))
-            high_info.insert(0, float(data["Time Series (Monthly)"][startDate]["2. high"]))
-            low_info.insert(0, float(data["Time Series (Monthly)"][startDate]["3. low"]))
-            close_info.insert(0, float(data["Time Series (Monthly)"][startDate]["4. close"]))
+            for d in data["Monthly Adjusted Time Series"]:
+                if d >= startDate and d <= endDate:
+                    time_info.insert(0, d)
+                    open_info.insert(0, float(data["Monthly Adjusted Time Series"][d]["1. open"]))
+                    high_info.insert(0, float(data["Monthly Adjusted Time Series"][d]["2. high"]))
+                    low_info.insert(0, float(data["Monthly Adjusted Time Series"][d]["3. low"]))
+                    close_info.insert(0, float(data["Monthly Adjusted Time Series"][d]["4. close"]))
+            if chartType == "bar":
+                line_chart = pygal.Bar()
+                line_chart.title = f'Stock Data for {symbol}: {startDate} to {endDate}'
+                line_chart.x_labels = time_info
+                line_chart.add('Open', open_info)
+                line_chart.add('High', high_info)
+                line_chart.add('Low', low_info)
+                line_chart.add('Close', close_info)
+                line_chart = line_chart.render_data_uri()
+            elif chartType == "line":
+                line_chart = pygal.Line()
+                line_chart.title = f'Stock Data for {symbol}: {startDate} to {endDate}'
+                line_chart.x_labels = time_info
+                line_chart.add('Open', open_info)
+                line_chart.add('High', high_info)
+                line_chart.add('Low', low_info)
+                line_chart.add('Close', close_info)
+                line_chart = line_chart.render_data_uri()
         return render_template('index.html', sdata=read_file(), chart=line_chart, symbol=symbol, chartType=chartType, timeSeries=timeSeries, startDate=startDate, endDate=endDate)
     return render_template('index.html', sdata=read_file())
 
